52deaf3bcf3310cd409ebce7ce7c9a22416dc31f
bakhtechsolutions <111666889+bakhtechsolutions@users.noreply.github.com>
2022-12-01 05:04:41 -0800

    de

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    sum = 0
-
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            sum += int(arg)
-    print("{}".format(sum))
